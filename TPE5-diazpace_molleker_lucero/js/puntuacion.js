5fb67c003d45c52bda4f731dd3524b5e2cf08cdd
Leonardo Molleker <lmolleker096@gmail.com>
2020-11-26 18:35:18 -0300

    Pages modifications

---
+++ TPE5-diazpace_molleker_lucero/js/puntuacion.js
@@ -0,0 +1,8 @@
+let estrellas = document.querySelectorAll('.score')
+
+function marcarEstrellas(numSection, numStar){
+    let stars = estrellas[numSection]
+    for(let i = 0; i<numStar; i++){
+        
+    }
+}

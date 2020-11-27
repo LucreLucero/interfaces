3a0b244e7e08a84c3fb3f8fea675ebf03fa2b540
Leonardo Molleker <lmolleker096@gmail.com>
2020-11-26 21:22:57 -0300

    Estrellas para votacion

--- TPE5-diazpace_molleker_lucero/js/puntuacion.js
+++ TPE5-diazpace_molleker_lucero/js/puntuacion.js
@@ -1,8 +1,5 @@
-let estrellas = document.querySelectorAll('.score')
-
 function marcarEstrellas(numSection, numStar){
-    let stars = estrellas[numSection]
-    for(let i = 0; i<numStar; i++){
-        
-    }
+    let section = document.querySelectorAll('.score')[numSection]
+    let estrellas = section.querySelectorAll('.star')
+    console.log(estrellas)
 }
